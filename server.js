651f08a01911b9893fdb2f99ddad0315f40026e4
OlegPanadiy <opanadiy@gmail.com>
2023-12-23 21:12:41 +0000

    commit

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-import app from "./app.js";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+import app from "./app.js";
+const DB_HOST =
+  "mongodb+srv://Random_user:NSalnEpBoDsoqqYG@cluster0.ukwzek5.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
